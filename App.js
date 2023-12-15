97ac447737f31210dc5bef61f66c2d9329896e73
hungnq <hungnq@jits.com.vn>
2023-12-15 18:27:43 +0700

    Add new code

---
+++ App.js
@@ -0,0 +1,13 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import { Button } from 'react-native-paper';
+import "react-native-gesture-handler"
+import Navigation from './navigation';
+
+export default function App() {
+  return (
+    <Navigation />
+  );
+}
+
+
